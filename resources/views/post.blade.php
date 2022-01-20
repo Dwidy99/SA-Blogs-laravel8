96b9fe9d00649f68cd62c0d5e1d4634ff2a727ec
Dwi <98dwiyulianto@gmail.com>
2022-01-20 20:20:54 +0700

    10. SearchingPagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,7 @@
                 <h1>{{ $post->title }}</h1>
                 
                 <p>
-                    By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none mb-3">
+                    By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none mb-3">
                         {{ $post->category->name }}
                     </a>.
                 </p>
