45f9097c9efe956e5ec27392151a9ca0651b30a6
Dwi <98dwiyulianto@gmail.com>
2022-01-25 19:37:44 +0700

    15. Create Post Form

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardPostController extends Controller
 {
@@ -27,7 +29,10 @@ class DashboardPostController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.posts.create', [
+            "title" => "Create New Post",
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -87,5 +92,11 @@ class DashboardPostController extends Controller
     public function destroy(Post $post)
     {
         //
+    }
+
+    public function checkSlug(Request $request)
+    {
+        $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
+        return response()->json(['slug' => $slug]);
     }
 }
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -2,10 +2,12 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
+    <h1 class="h2">{{ $title }}</h1>
 </div>
 
+
 <div class="table-responsive">
+    <a href="/dashboard/posts/create" class="btn btn-primary">Create New Post</a>
     <table class="table table-striped table-sm">
         <thead>
         <tr>
